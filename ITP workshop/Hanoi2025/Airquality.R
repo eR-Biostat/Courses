07f4da8e11e9d4cf0650125e05dc35f13b3f72db
erbiostat <ziv.shkedy@uhasselt.be>
2025-02-27 15:50:43 +0100

    Delete ITP workshop/Hanoi2025/Airquality.R

--- ITP workshop/Hanoi2025/Airquality.R
+++
@@ -1,9 +0,0 @@
-wind<-airquality$Wind
-M.wind<-mean(wind)
-SD.wind<-sqrt(var(wind))
-M.wind
-SD.wind
-wind=na.omit(airquality$Wind)
-library(TeachingDemos)
-z.test(wind,sd=SD.wind)
-z.test(wind, SD.wind, mu=9)
